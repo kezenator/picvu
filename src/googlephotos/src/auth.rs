65acc09ae4c55ada2376d777a0a5973e3504b885
kezenator <github@kezenator.com>
2022-01-29 07:38:38 +1000

    Updated final difficult cargo crates

--- src/googlephotos/src/auth.rs
+++ src/googlephotos/src/auth.rs
@@ -1,8 +1,12 @@
-use oauth2::prelude::*;
-use oauth2::{AuthorizationCode, AuthUrl, ClientId, ClientSecret, CsrfToken,
-    RedirectUrl, RequestTokenError, ResponseType,
-    Scope, StandardTokenResponse, TokenResponse, TokenUrl};
+use oauth2::{
+    AuthorizationCode, AuthUrl,
+    ClientId, ClientSecret, CsrfToken,
+    PkceCodeChallenge, PkceCodeVerifier,
+    RedirectUrl, ResponseType,
+    Scope, StandardTokenResponse,
+    TokenResponse, TokenUrl};
 use oauth2::basic::BasicClient;
+use oauth2::reqwest::http_client;
 use url::Url;
 
 pub struct GoogleAuthSetup
@@ -15,8 +19,8 @@ pub struct GoogleAuthSetup
 enum ClientState
 {
     None,
-    StartedNew{client: BasicClient, csrf_token: CsrfToken},
-    GotCallback{client: BasicClient, code: AuthorizationCode},
+    StartedNew{client: BasicClient, csrf_token: CsrfToken, pkce_verifier: PkceCodeVerifier},
+    GotCallback{client: BasicClient, code: AuthorizationCode, pkce_verifier: PkceCodeVerifier},
     Error{err: String},
     Done{access_token: AccessToken},
 }
@@ -53,24 +57,34 @@ impl GoogleAuthClient
         let client = BasicClient::new(
             ClientId::new(setup.client_id),
             Some(ClientSecret::new(setup.client_secret)),
-            AuthUrl::new(Url::parse("https://accounts.google.com/o/oauth2/v2/auth").unwrap()),
-            Some(TokenUrl::new(Url::parse("https://oauth2.googleapis.com/token").unwrap())))
+            AuthUrl::from_url(Url::parse("https://accounts.google.com/o/oauth2/v2/auth").unwrap()),
+            Some(TokenUrl::from_url(Url::parse("https://oauth2.googleapis.com/token").unwrap())))
+            .set_redirect_uri(RedirectUrl::from_url(Url::parse(&setup.redirect_url).unwrap()));
+
+        // Generate a PKCE challenge.
+        let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
+
+        // Generate the full authorization URL.
+        let (auth_url, csrf_token) = client
+            .authorize_url(CsrfToken::new_random)
+            // Set the desired scopes.
             .add_scope(Scope::new("https://www.googleapis.com/auth/photoslibrary".to_string()))
-            .set_redirect_url(RedirectUrl::new(Url::parse(&setup.redirect_url).unwrap()));
+            // Set the PKCE code challenge.
+            .set_pkce_challenge(pkce_challenge)
+            // Set response type and extensions
+            .set_response_type(&ResponseType::new("code".to_owned()))
+            .add_extra_param("access_type", "offline")
+            .add_extra_param("include_granted_scopes", "true")
+            .url();
 
-        let response_type = ResponseType::new("code".to_owned());
-        let extensions = [("access_type", "offline"), ("include_granted_scopes", "true")];
-
-        let (auth_url, csrf_token) = client.authorize_url_extension(&response_type, CsrfToken::new_random, &extensions);
-
-        self.state = ClientState::StartedNew{ client, csrf_token };
+        self.state = ClientState::StartedNew{ client, csrf_token, pkce_verifier };
 
         auth_url.to_string()
     }
 
     pub fn got_callback(&mut self, code: Option<String>, state: Option<String>, error: Option<String>)
     {
-        if let ClientState::StartedNew{client, csrf_token} = &self.state
+        if let ClientState::StartedNew{client, csrf_token, pkce_verifier} = &self.state
         {
             if let Some(code) = code
             {
@@ -80,7 +94,10 @@ impl GoogleAuthClient
                     {
                         let code = AuthorizationCode::new(code);
 
-                        self.state = ClientState::GotCallback{ client: client.clone(), code };
+                        self.state = ClientState::GotCallback{
+                            client: client.clone(),
+                            code: code,
+                            pkce_verifier: PkceCodeVerifier::new(pkce_verifier.secret().clone()) };
                     }
                     else
                     {
@@ -101,9 +118,12 @@ impl GoogleAuthClient
 
     pub fn exchange_token(&mut self) -> ExchangeOperation
     {
-        if let ClientState::GotCallback{client, code} = &self.state
+        if let ClientState::GotCallback{client, code, pkce_verifier} = &self.state
         {
-            return ExchangeOperation::new(client.clone(), code.clone());
+            return ExchangeOperation::new(
+                client.clone(),
+                code.clone(),
+                PkceCodeVerifier::new(pkce_verifier.secret().clone()));
         }
         else if let ClientState::Error{err} = &self.state
         {
@@ -123,15 +143,15 @@ impl GoogleAuthClient
 
 pub enum ExchangeOperation
 {
-    Continue{ client: BasicClient, code: AuthorizationCode },
+    Continue{ client: BasicClient, code: AuthorizationCode, pkce_verifier: PkceCodeVerifier },
     Error{ err: String },
 }
 
 impl ExchangeOperation
 {
-    fn new(client: BasicClient, code: AuthorizationCode) -> Self
+    fn new(client: BasicClient, code: AuthorizationCode, pkce_verifier: PkceCodeVerifier) -> Self
     {
-        ExchangeOperation::Continue{client, code}
+        ExchangeOperation::Continue{client, code, pkce_verifier}
     }
 
     fn error(err: String) -> Self
@@ -143,10 +163,12 @@ impl ExchangeOperation
     {
         match self
         {
-            ExchangeOperation::Continue{client, code} =>
+            ExchangeOperation::Continue{client, code, pkce_verifier} =>
             {
                 client.exchange_code(code)
-                    .map_err(|err| GoogleAuthError::Oauth2{err})
+                    .set_pkce_verifier(pkce_verifier)
+                    .request(http_client)
+                    .map_err(|err| GoogleAuthError::Oauth2{err: format!("{}", err)})
                     .map(|response| GoogleAuthTokenReponse{response})
             },
             ExchangeOperation::Error{err} =>
@@ -161,7 +183,7 @@ impl ExchangeOperation
 pub enum GoogleAuthError
 {
     Other{err: String},
-    Oauth2{err: RequestTokenError<oauth2::basic::BasicErrorResponseType>},
+    Oauth2{err: String},
 }
 
 #[derive(Debug)]
